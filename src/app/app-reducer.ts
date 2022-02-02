d85837dc402c1a48540f23d01c97330f50c0e7c6
Beergrizl <a.sadovskii@tut.by>
2022-02-02 23:13:39 +0300

    Initial commit

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,58 @@
+import {Dispatch} from "redux";
+import {setIsLoggedInAC} from "../features/Login/auth-reducer";
+import {authAPI} from "../api/todolists-api";
+
+const initialState: InitialStateType = {
+    status: 'idle',
+    error: null,
+    isInitialized: false
+}
+
+export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
+        case "APP/IS-INITIALISED":
+            return {...state, isInitialized: action.isInitialized}
+        default:
+            return {...state}
+    }
+}
+
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+export type InitialStateType = {
+    status: RequestStatusType
+    error: string | null,
+    isInitialized: boolean
+}
+
+export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
+export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const)
+export const isInitializedAC = (isInitialized: boolean) => ({type: 'APP/IS-INITIALISED', isInitialized} as const)
+
+export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
+export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+export type isInitializedActionType = ReturnType<typeof isInitializedAC>
+
+type ActionsType =
+    | SetAppErrorActionType
+    | SetAppStatusActionType
+    | isInitializedActionType
+
+export const initializeAppTC = () => (dispatch: Dispatch) => {
+    authAPI.me().then(res => {
+        debugger
+        if (res.data.resultCode === 0) {
+            dispatch(setIsLoggedInAC(true));
+
+
+        } else {
+        }
+    })
+        .finally(() => {
+            dispatch(isInitializedAC(true));
+        })
+}
+
